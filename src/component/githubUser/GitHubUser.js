844aa050939d8e1ea5b804b63a2d6750a8a3f236
cssong <a1373052011@gmail.com>
2022-04-03 13:12:42 +0900

    프로미스 상태 처리

--- src/component/githubUser/GitHubUser.js
+++ src/component/githubUser/GitHubUser.js
@@ -4,13 +4,14 @@ import { loadJSON, saveJSON } from "../common/Storage"
 export default function GitHubUser({ login }) {
     const key = `user:${login}`;
     const [data, setData] = useState(loadJSON(key));
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
 
     console.log('data', data);
 
     useEffect(() => {
         if(!data) return;
         if(data.login === login) return;
-
         console.log("run saveJSON");
         const { name, avatar_url, location } = data;
         saveJSON(key, {
@@ -24,16 +25,33 @@ export default function GitHubUser({ login }) {
     useEffect(() => {
         if(!login) return;
         if(data && data.login === login) return;
-
         console.log("run fetch");
+        setLoading(true);
         fetch(`https://api.github.com/users/${login}`)
             .then(response => response.json())
             .then(setData)
+            .then(() => setLoading(false))
             .catch(console.error);
     }, [login]);
 
-    if(data)
-        return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    if(loading) return <h1>loading...</h1>;
+    if(error)
+        return <pre>{JSON.stringify(error, null, 2)}</pre>;
+    if(!data)
+        return null;
 
-    return null;
+    return (
+        <div className="githubUser">
+            <img
+                src={data.avatar_url}
+                alt={data.login}
+                style={{ width: 200 }}
+            />
+            <div>
+                <h1>{data.login}</h1>
+                {data.name && <p>{data.name}</p>}
+                {data.location && <p>{data.location}</p>}
+            </div>
+        </div>
+    );
 }
