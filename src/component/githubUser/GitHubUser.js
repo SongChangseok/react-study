a0b4edf8362d357db2e12c313e397a7d7c67a3f5
cssong <a1373052011@gmail.com>
2022-04-06 22:25:06 +0900

    fetch컴포넌트 변경 예제

--- src/component/githubUser/GitHubUser.js
+++ src/component/githubUser/GitHubUser.js
@@ -1,51 +1,78 @@
 import React, { useEffect, useState } from "react";
+import { Fetch } from "../common/Fetch";
 import { loadJSON, saveJSON } from "../common/Storage"
 
+// export default function GitHubUser({ login }) {
+//     const key = `user:${login}`;
+//     const [data, setData] = useState(loadJSON(key));
+//     const [error, setError] = useState();
+//     const [loading, setLoading] = useState(false);
+
+//     console.log('data', data);
+
+//     useEffect(() => {
+//         if(!data) return;
+//         if(data.login === login) return;
+//         console.log("run saveJSON");
+//         const { name, avatar_url, location } = data;
+//         saveJSON(key, {
+//             name,
+//             login,
+//             avatar_url,
+//             location
+//         });
+//     }, [data]);
+
+//     useEffect(() => {
+//         if(!login) return;
+//         if(data && data.login === login) return;
+//         console.log("run fetch");
+//         setLoading(true);
+//         fetch(`https://api.github.com/users/${login}`)
+//             .then(response => response.json())
+//             .then(setData)
+//             .then(() => setLoading(false))
+//             .catch(console.error);
+//     }, [login]);
+
+//     if(loading) return <h1>loading...</h1>;
+//     if(error)
+//         return <pre>{JSON.stringify(error, null, 2)}</pre>;
+//     if(!data)
+//         return null;
+
+//     return (
+//         <div className="githubUser">
+//             <img
+//                 src={data.avatar_url}
+//                 alt={data.login}
+//                 style={{ width: 200 }}
+//             />
+//             <div>
+//                 <h1>{data.login}</h1>
+//                 {data.name && <p>{data.name}</p>}
+//                 {data.location && <p>{data.location}</p>}
+//             </div>
+//         </div>
+//     );
+// }
+
 export default function GitHubUser({ login }) {
-    const key = `user:${login}`;
-    const [data, setData] = useState(loadJSON(key));
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState(false);
-
-    console.log('data', data);
-
-    useEffect(() => {
-        if(!data) return;
-        if(data.login === login) return;
-        console.log("run saveJSON");
-        const { name, avatar_url, location } = data;
-        saveJSON(key, {
-            name,
-            login,
-            avatar_url,
-            location
-        });
-    }, [data]);
-
-    useEffect(() => {
-        if(!login) return;
-        if(data && data.login === login) return;
-        console.log("run fetch");
-        setLoading(true);
-        fetch(`https://api.github.com/users/${login}`)
-            .then(response => response.json())
-            .then(setData)
-            .then(() => setLoading(false))
-            .catch(console.error);
-    }, [login]);
-
-    if(loading) return <h1>loading...</h1>;
-    if(error)
-        return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    if(!data)
-        return null;
-
     return (
-        <div className="githubUser">
+        <Fetch
+            uri={`https://api.github.com/users/${login}`}
+            renderSuccess={UserDetails}
+        />
+    );
+}
+
+function UserDetails({ data }) {
+    return (
+        <div>
             <img
                 src={data.avatar_url}
                 alt={data.login}
-                style={{ width: 200 }}
+                style={{ witdh: 200 }}
             />
             <div>
                 <h1>{data.login}</h1>
@@ -53,5 +80,5 @@ export default function GitHubUser({ login }) {
                 {data.location && <p>{data.location}</p>}
             </div>
         </div>
-    );
+    )
 }
