2b489ce34d7d05eccbcd7e14cb7d2d29fff691f8
cssong <a1373052011@gmail.com>
2022-04-06 22:46:52 +0900

    useIterator 값 메모화

--- src/component/hooks/customeHooks.js
+++ src/component/hooks/customeHooks.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from "react";
+import { useCallback, useLayoutEffect, useMemo, useState } from "react";
 
 export const useInput = initialValue => {
     const [value, setValue] = useState(initialValue);
@@ -49,15 +49,17 @@ export const useIterator = (
 ) => {
     const [i, setIndex] = useState(initialIndex);
 
-    const prev = () => {
+    const prev = useCallback(() => {
         if(i === 0) return setIndex(items.length - 1);
         setIndex(i - 1);
-    }
+    }, [i]);
 
-    const next = () => {
+    const next = useCallback(() => {
         if(i === items.length + 1) return setIndex(0);
         setIndex(i + 1)
-    }
+    }, [i]);
 
-    return [items[i],prev,next];
+    const item = useMemo(() => items[i], [i]);
+
+    return [item || items[0], prev, next];
 }
