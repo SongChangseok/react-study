7b742c50ff4f0137e95eef841c9b442764955e94
cssong <a1373052011@gmail.com>
2022-04-06 22:43:07 +0900

    useIterator 생성 - 원형 배열

--- src/component/hooks/customeHooks.js
+++ src/component/hooks/customeHooks.js
@@ -42,3 +42,22 @@ export const useMousePosition = () => {
 
     return [x, y];
 }
+
+export const useIterator = (
+    items = [],
+    initialIndex = 0
+) => {
+    const [i, setIndex] = useState(initialIndex);
+
+    const prev = () => {
+        if(i === 0) return setIndex(items.length - 1);
+        setIndex(i - 1);
+    }
+
+    const next = () => {
+        if(i === items.length + 1) return setIndex(0);
+        setIndex(i + 1)
+    }
+
+    return [items[i],prev,next];
+}
