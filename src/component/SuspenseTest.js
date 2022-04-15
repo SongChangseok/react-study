38376f19b712746c41ecf06f3c5db5e873777b28
cssong <a1373052011@gmail.com>
2022-04-15 17:55:17 +0900

    ErrorBoundary를 이용한 적재 실패에 대한 처리

--- src/component/SuspenseTest.js
+++ src/component/SuspenseTest.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense, useState } from "react";
 import Agreement from "./suspenseTest/Agreement";
 // import Main from "./suspenseTest/Main"
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import ErrorBoundary from "./suspenseTest/ErrorBoundary";
 
 const Main = lazy(() => import("./suspenseTest/Main"));
 
@@ -12,8 +13,10 @@ export default function SuspenseTest() {
         return <Agreement onAgree={() => setAgree(true)} />;
 
     return (
-        <Suspense fallback={<ClimbingBoxLoader/>}>
-            <Main/>
-        </Suspense>
+        <ErrorBoundary>
+            <Suspense fallback={<ClimbingBoxLoader/>}>
+                <Main/>
+            </Suspense>
+        </ErrorBoundary>
     );
 }
