edeb1912b8dc49ca6cbd76d66003d3c8fe27156b
cssong <a1373052011@gmail.com>
2022-04-18 23:15:26 +0900

    쿼리 테스트

--- src/component/star/Star.test.js
+++ src/component/star/Star.test.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import Star from "./Star";
@@ -15,3 +16,8 @@ import Star from "./Star";
 //   expect(div.querySelector("svg")).toHaveClass("test");
 // });
 
+test("renders an h1", () => {
+  const { getByText } = render(<Star />);
+  const h1 = getByText(/Star/);
+  expect(h1).toHaveTextContent("Star");
+});
