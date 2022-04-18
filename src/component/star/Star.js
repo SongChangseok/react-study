6271e0dbfe7342b8be8e6195b951b8ba55584727
cssong <a1373052011@gmail.com>
2022-04-18 17:50:08 +0900

    jest를 이용한 컴포넌트 test

--- src/component/star/Star.js
+++ src/component/star/Star.js
@@ -2,5 +2,7 @@ import React from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function Star({ selected = false, onSelect = (f) => f }) {
-  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
+  return (
+    <FaStar id="star" color={selected ? "red" : "grey"} onClick={onSelect} />
+  );
 }
---
+++ src/component/star/Star.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Star from "./Star";
+
+// test("renders a start", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<Star />, div);
+//   expect(div.querySelector("svg")).toBeTruthy();
+// });
+
+test("renders a start", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Star />, div);
+  //   expect(div.querySelector("svg")).toHaveAttribute("id", "star");
+  expect(div.querySelector("svg")).toHaveClass("test");
+});
