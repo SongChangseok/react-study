022666e7429cda725c4dae5ddfb53cd6a89e7c06
cssong <a1373052011@gmail.com>
2022-04-19 15:53:40 +0900

    useRoutes를 이용한 라우팅

--- src/component/RouterTest.js
+++ src/component/RouterTest.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
+  //   Routes,
+  //   Route,
+  Navigate,
+  useRoutes
 } from "react-router-dom";
 import {
   About,
@@ -17,35 +18,57 @@ import {
   Whoops404
 } from "./website/pages";
 
-function Tree() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="services" element={<Services />} />
-          <Route path="history" element={<History />} />
-          <Route path="location" element={<Location />} />
-        </Route>
-        <Route path="/events" element={<Events />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Whoops404 />} />
+// function Tree() {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />}>
+//           <Route path="services" element={<Services />} />
+//           <Route path="history" element={<History />} />
+//           <Route path="location" element={<Location />} />
+//         </Route>
+//         <Route path="/events" element={<Events />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="*" element={<Whoops404 />} />
 
-        {/* <Redirect from="services" to="about/services" /> v6부터 Redirect 삭제됨, Navigate로 변경됨 */}
-        <Route
-          path="services"
-          element={<Navigate to="about/services" replace />}
-        />
-      </Routes>
-    </div>
-  );
+//         {/* <Redirect from="services" to="about/services" /> v6부터 Redirect 삭제됨, Navigate로 변경됨 */}
+//         <Route
+//           path="services"
+//           element={<Navigate to="about/services" replace />}
+//         />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+function Tree2() {
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    {
+      path: "about",
+      element: <About />,
+      children: [
+        { path: "services", element: <Services /> },
+        { path: "history", element: <History /> },
+        { path: "location", element: <Location /> }
+      ]
+    },
+    { path: "events", element: <Events /> },
+    { path: "products", element: <Products /> },
+    { path: "contact", element: <Contact /> },
+    { path: "*", element: <Whoops404 /> },
+    { path: "services", element: <Navigate to="about/services" replace /> }
+  ]);
+
+  return element;
 }
 
 export default function RouterTest() {
   return (
     <Router>
-      <Tree />
+      <Tree2 />
     </Router>
   );
 }
