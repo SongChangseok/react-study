4823ad8ebfbd899018726a050083e385c91739c9
cssong <a1373052011@gmail.com>
2022-04-19 15:32:25 +0900

    v6부터 Redirect가 삭제되고 Navigate로 대체됨

--- src/component/RouterTest.js
+++ src/component/RouterTest.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 import {
   About,
   Contact,
   Events,
+  History,
   Home,
+  Location,
   Products,
+  Services,
   Whoops404
 } from "./website/pages";
 
@@ -14,11 +22,21 @@ function Tree() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<History />} />
+          <Route path="location" element={<Location />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Whoops404 />} />
+
+        {/* <Redirect from="services" to="about/services" /> v6부터 Redirect 삭제됨, Navigate로 변경됨 */}
+        <Route
+          path="services"
+          element={<Navigate to="about/services" replace />}
+        />
       </Routes>
     </div>
   );
--- src/component/website/pages.js
+++ src/component/website/pages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import "./pages.css";
 
@@ -21,6 +21,7 @@ export function About() {
   return (
     <div>
       <h1>[회사소개]</h1>
+      <Outlet />
     </div>
   );
 }
@@ -53,8 +54,59 @@ export function Whoops404() {
   let location = useLocation();
   console.log(location);
   return (
-    <div>
+    <div className="whoops-404">
       <h1>Resource not found at {location.pathname}</h1>
+    </div>
+  );
+}
+
+export function Services() {
+  return (
+    <div>
+      <h2>Our Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+        mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+        sodales ligula in libero.
+      </p>
+    </div>
+  );
+}
+
+export function History() {
+  return (
+    <div>
+      <h2>Our Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+        mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+        sodales ligula in libero.
+      </p>
+    </div>
+  );
+}
+
+export function Location() {
+  return (
+    <div>
+      <h2>Our Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+        mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+        sodales ligula in libero.
+      </p>
     </div>
   );
 }
